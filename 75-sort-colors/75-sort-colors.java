c9c66d7043db3fd3a92ee475b95f8dfb7b36bed6
yashj-7 <63853210+yashj-7@users.noreply.github.com>
2022-01-07 15:42:23 +0530

    Time: 1 ms (19.61%), Space: 39.4 MB (9.50%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -1,48 +1,26 @@
 class Solution {
     public void sortColors(int[] nums) {
-        mergeSort(nums,0,nums.length-1);
-    }
-    public static void mergeSort(int[] a,int beg,int end){
-        if(beg<end){
-             int mid = (beg+end)/2;
-            mergeSort(a,beg,mid);
-            mergeSort(a,mid+1,end);
-            merge(a,beg,mid,end);
-        }
-    }
-    public static void merge(int[] a,int start,int mid,int end){
-        
-        int[] arr = new int[end-start+1];
-        int i = start;
-        int j = mid+1;
-        int k=0;
-        while(i<=mid && j<=end){
-            if(a[i]<=a[j]){
-                arr[k]=a[i];
-                i++;
-            }else if(a[i]>a[j]){
-                arr[k]=a[j];
-                j++;
-            }
-            k++;
-        }
-        if(i>mid && j<=end){
-            while(j<=end){
-                arr[k]=a[j];
-                j++;
-                k++;
-            }
-        }else if(i<=mid && j>end){
-            while(i<=mid){
-                arr[k]=a[i];
-                i++;
-                k++;
+        int max = 0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>max){
+                max=nums[i];
             }
         }
-        int y=0;
-        for(int l=start;l<=end;l++){
-            a[l]=arr[y];
-            y++;
+        int[] arr = new int[max+1];
+        Arrays.fill(arr,0);
+        for(int i=0;i<nums.length;i++){
+            arr[nums[i]] = arr[nums[i]]+1;
+        }
+        for(int i=1;i<arr.length;i++){
+            arr[i] = arr[i]+arr[i-1];
+        }
+        int[] a = new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            a[arr[nums[i]]-1] = nums[i];
+            arr[nums[i]] = arr[nums[i]]-1;
+        }
+        for(int i=0;i<nums.length;i++){
+            nums[i] = a[i];
         }
     }
 }
