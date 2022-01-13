090774eccea6e16a2ad93522e314e799ced722a4
yashj-7 <63853210+yashj-7@users.noreply.github.com>
2022-01-14 00:02:15 +0530

    Time: 3 ms (19.97%), Space: 39.8 MB (5.58%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -1,23 +1,44 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int temp = 0;
-        for(int i=0;i<matrix.length;i++){
-            if(matrix.length==1 || matrix[0][0]>target){
-                break;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int l = 0;
+        int r = matrix.length-1;
+        int point =-1;
+        while(l<=r){
+            int mid = (l+r)/2;
+            if(l==r){
+                point = l;
             }
-            if(i==matrix.length-1 && matrix[i][0]<target){
-                temp =i;
-                break;
-            }else if(matrix[i][0]>target){
-             temp = i-1;
-             break;
-         }else if(matrix[i][0]==target){
-             return true;
-         }
-        }
-        for(int i=0;i<matrix[temp].length;i++){
-            if(matrix[temp][i]==target){
+            if(matrix[mid][0]==target){
                 return true;
+            }else if(matrix[mid][0]>target){
+                r = mid-1;
+            }else if(matrix[mid][0]<target){
+                if(matrix[mid][matrix[mid].length-1]>=target){
+                    l = mid;
+                    r=mid;
+                    point = l;
+                    break;
+                }else{
+                    l=mid+1;
+                }
+            }
+        }
+        if(point==-1){
+            return false;
+        }
+        int left = 0;
+        int right = matrix[0].length-1;
+        System.out.println(point);
+        while(left<=right){
+            int mid = (left+right)/2;
+            if(matrix[point][mid]==target){
+                return true;
+            }else if(matrix[point][mid]<target){
+                left = mid+1;
+            }else if(matrix[point][mid]>target){
+                right = mid-1;
             }
         }
         return false;
