bfe91239dcda8b77e2345cf6b5e030bbeec55c7f
yashj-7 <63853210+yashj-7@users.noreply.github.com>
2022-01-27 14:36:33 +0530

    Time: 15 ms (64.82%), Space: 192.3 MB (5.00%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.java
+++ 131-palindrome-partitioning/131-palindrome-partitioning.java
@@ -17,6 +17,7 @@ class Solution {
                 temp.remove(temp.size()-1);
             }
         }
+        return;
     }
     public boolean isPalindrome(String s,int start,int end){
         while(start<=end){
