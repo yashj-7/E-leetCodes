95136718deeb051423ff2e214c649666f53b632f
yashj-7 <63853210+yashj-7@users.noreply.github.com>
2022-02-03 16:26:38 +0530

    Time: 0 ms (100.00%), Space: 60.3 MB (5.49%) - LeetHub

---
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int low = 0;
+        int high = nums.length-1;
+        if(nums.length==1){
+            return nums[nums.length-1];
+        }
+        while(low<high){
+            int mid = low+(high-low)/2;
+            if(mid%2==0 && nums[mid+1]==nums[mid]){
+                low = mid+1;
+            }else if(mid%2!=0 && nums[mid-1]==nums[mid]){
+                low=mid+1;
+            }else{
+                high = mid;
+            }
+        }
+        return nums[low];
+    }
+}
