f88d996ca40bbc64006a2ceb332de8ce05bfb6b8
yashj-7 <63853210+yashj-7@users.noreply.github.com>
2022-02-08 18:18:37 +0530

    Time: 3 ms (74.03%), Space: 43.8 MB (5.87%) - LeetHub

---
+++ 994-rotting-oranges/994-rotting-oranges.java
@@ -0,0 +1,61 @@
+class Solution {
+    public int orangesRotting(int[][] grid) {
+        Queue<int[]> q = new LinkedList<>();
+        int count = 0;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+             if(grid[i][j]==2){
+                 q.add(new int[]{i,j});
+                 count++;
+             }else if(grid[i][j]==1){
+                 count++;
+             }   
+            }
+        }
+        int min = 0;
+        int flag = q.size();
+       // System.out.println(flag);
+       // System.out.print(q.peek()[1]);
+        while(q.isEmpty()==false){
+            ArrayList<int[]> list = new ArrayList();
+            while(!q.isEmpty()){
+                list.add(q.remove());
+            }
+            //System.out.println("list size is "+list.size());
+            for(int i=0;i<list.size();i++){
+                int x = list.get(i)[0];
+                int y = list.get(i)[1];
+                if(x-1>=0 && grid[x-1][y]==1){
+                    q.offer(new int[]{x-1,y});
+                    grid[x-1][y] =2;
+                    flag++;
+                }
+                if(y-1>=0 && grid[x][y-1]==1){
+                    q.offer(new int[]{x,y-1});
+                    grid[x][y-1] =2;
+                    flag++;
+                }
+                if(y+1<grid[0].length && grid[x][y+1]==1){
+                    q.offer(new int[]{x,y+1});
+                    grid[x][y+1]=2;
+                    flag++;
+                }
+                if(x+1<grid.length && grid[x+1][y]==1){
+                    q.offer(new int[]{x+1,y});
+                    grid[x+1][y]=2;
+                    flag++;
+                }
+                 
+                //System.out.println(min);
+            }
+            if(q.isEmpty()==false){
+                    min++;
+                }
+        }
+        if(flag<count){
+            return -1;
+        }
+        
+        return min;
+    }
+}
