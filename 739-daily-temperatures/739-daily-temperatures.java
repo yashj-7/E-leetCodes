8bd8dda51bfc228caa9b15a321dbc3c6f323ba4f
yashj-7 <63853210+yashj-7@users.noreply.github.com>
2022-02-09 21:48:17 +0530

    Time: 221 ms (12.94%), Space: 129.4 MB (11.65%) - LeetHub

---
+++ 739-daily-temperatures/739-daily-temperatures.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        
+        class helper{
+            int val;
+            int index;
+            helper(int val,int index){
+                this.val = val;
+                this.index = index;
+            }
+        }
+        
+        Stack<helper> s = new Stack();
+        int[] arr = new int[temperatures.length];
+        for(int i=0;i<temperatures.length;i++){
+            if(i==0){
+                s.push(new helper(temperatures[i],i));
+            }else{
+                if(!s.isEmpty() && temperatures[i]<s.peek().val){
+                    s.push(new helper(temperatures[i],i));
+                }else{
+                    while(s.isEmpty()==false && temperatures[i]>s.peek().val){
+                        arr[s.peek().index]=i-s.peek().index;
+                        s.pop();
+                    }
+                    s.push(new helper(temperatures[i],i));
+                }
+            }
+        }
+        while(s.isEmpty()==false){
+            arr[s.pop().index]=0;
+        }
+        return arr;
+    }
+}
