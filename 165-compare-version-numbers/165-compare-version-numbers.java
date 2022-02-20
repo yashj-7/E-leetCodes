29bd8bc5c7ec57abbd2448114e5c419a42ba6deb
yashj-7 <63853210+yashj-7@users.noreply.github.com>
2022-02-20 10:36:33 +0530

    Time: 8 ms (7.24%), Space: 41 MB (32.36%) - LeetHub

---
+++ 165-compare-version-numbers/165-compare-version-numbers.java
@@ -0,0 +1,60 @@
+class Solution {
+    public int compareVersion(String version1, String version2) {
+       String[] v1 = version1.split("[.]");
+        String[] v2 = version2.split("[.]");
+        //System.out.print(v1.length);
+        for(int i=0;i<v1.length;i++){
+            System.out.print(v1[i]+" ");
+        }
+        System.out.println();
+        for(int i=0;i<v2.length;i++){
+            System.out.print(v2[i]+" ");
+        }
+        int i =0;
+        int j=0;
+        while(i<v1.length && j<v2.length){
+            int x = Integer.parseInt(v1[i]);
+            int y = Integer.parseInt(v2[i]);
+            if(x<y){
+                return -1;
+            }else if(x>y){
+                return 1;
+            }else{
+                if(i==v1.length-1 && j==v2.length-1){
+                    return 0;
+                }
+            }
+            i++;
+            j++;
+        }
+        if(i==v1.length && j<v2.length){
+            while(j<v2.length){
+                int x =0;
+            int y = Integer.parseInt(v2[j]);
+            if(x<y){
+                return -1;
+            }else if(x==y){
+                if(j==v2.length-1){
+                    return 0;
+                }
+            }
+            j++;   
+            }
+        }
+        if(i!=v1.length && j==v2.length){
+            while(i<v1.length){
+                int x =Integer.parseInt(v1[i]);
+            int y = 0;
+            if(x>y){
+                return 1;
+            }else if(x==y){
+                if(i==v1.length-1){
+                    return 0;
+                }
+            }
+            i++;   
+            }
+        }
+       return 1;
+    }
+}
