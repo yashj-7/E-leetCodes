a98c04a5ae78c31a60846195f5461876a16288aa
yashj-7 <63853210+yashj-7@users.noreply.github.com>
2022-02-26 15:07:41 +0530

    Added solution - LeetHub

---
+++ Top View of Binary Tree - GFG/top-view-of-binary-tree.java
@@ -0,0 +1,176 @@
+// { Driver Code Starts
+//Initial Template for JAVA
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+public class Tree {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root)
+    {
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+    	    	
+                Solution ob = new Solution();
+
+                ArrayList<Integer> vec = ob.topView(root);
+                for(int x : vec)
+                    System.out.print(x + " ");
+                System.out.println();
+        	
+                t--;   
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+*/
+
+class Solution
+{
+    static class Triplet{
+        int col;
+        int row;
+        Node n;
+        Triplet(int col,int row,Node n){
+            this.col = col;
+            this.row = row;
+            this.n = n;
+        }
+    }
+    static ArrayList<Integer> topView(Node root)
+    {
+        ArrayList<Integer> list = new ArrayList<>();
+        TreeMap<Integer,TreeMap<Integer,Integer>> map = new TreeMap<>();
+        Queue<Triplet> q = new LinkedList<>();
+        q.offer(new Triplet(0,0,root));
+        
+        // TreeMap<Integer,ArrayList<Integer>> mxp = new TreeMap<>();
+        // ArrayList<Integer> x = new ArrayList<>();
+        // x.add(root.data);
+        // mxp.put(0,x);
+        // map.put(0,mxp);
+        
+        while(q.isEmpty()==false){
+            Triplet temp = q.poll();
+            if(temp.n.left!=null){
+                q.offer(new Triplet(temp.col-1,temp.row+1,temp.n.left));
+            }
+            if(temp.n.right!=null){
+                q.offer(new Triplet(temp.col+1,temp.row+1,temp.n.right));
+            }
+            
+            if(map.containsKey(temp.col)==false){
+                    TreeMap<Integer,Integer> mp = new TreeMap<>();
+                    mp.put(temp.row,temp.n.data);
+                    map.put(temp.col,mp);
+                }
+            
+        }
+         //System.out.print(map);
+        while(map.size()>0){
+            int key = map.firstKey();
+            int k = map.get(key).firstKey();
+            list.add(map.get(key).get(k));
+            map.remove(key);
+        }
+        return list;
+    }
+}
