16a4112d53a2850ee253526b4d59338f32ffaab5
yashj-7 <63853210+yashj-7@users.noreply.github.com>
2022-03-09 19:13:50 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
@@ -54,23 +54,22 @@ class Solution
          int[][] dp = new int[n+1][w+1];
         for(int i=0;i<n+1;i++){
             for(int j=0;j<w+1;j++){
-                dp[i][j] = -1;
+                if(i==0||j==0){
+                    dp[i][j] = 0;
+                }
             }
         }
-        return solve(w,wt,val,n,dp);
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<w+1;j++){
+                if(wt[i-1]>j){
+                    dp[i][j] = dp[i-1][j];
+                }else{
+                    dp[i][j] = Math.max(val[i-1]+dp[i-1][j-wt[i-1]],dp[i-1][j]);
+                }
+            }
+        }
+        return dp[n][w];
     
-    }
-    public static int solve(int w,int[]wt,int val[],int n,int[][] dp){
-        if(n==0 || w==0){
-             return 0;
-         }
-         if(dp[n][w]!=-1){
-             return dp[n][w];
-         }
-         if(wt[n-1]>w){
-             return dp[n][w]=solve(w,wt,val,n-1,dp);
-         }
-         return dp[n][w]=Math.max(val[n-1]+solve(w-wt[n-1],wt,val,n-1,dp),solve(w,wt,val,n-1,dp));
     }
     // public void solve(int[] wt,int[] val,int w,int n)
 }
