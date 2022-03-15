4f0c73c1dee0266cefc35433ec70371a8bf36df5
yashj-7 <63853210+yashj-7@users.noreply.github.com>
2022-03-15 20:04:58 +0530

    Time: 6 ms (12.43%), Space: 46.5 MB (13.39%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.java
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -4,22 +4,6 @@ class Solution {
         return solve(grid,dp);
     }
     public int solve(int[][] grid,int[][] dp){
-//         if(i==0 && j==0){
-//             return grid[i][j];
-//         }
-//         if(i<0 || j<0){
-//             return (int)1e9;
-//         }
-//         if(dp[i][j]!=-1){
-//             return dp[i][j];
-//         }
-//          int up = grid[i][j] + solve(i-1,j,grid,dp);
-//          int left = grid[i][j] + solve(i,j-1,grid,dp);
-        
-//         return dp[i][j]= Math.min(left,up);
-        
-        
-        //dp[0][0] = grid[0][0];
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length;j++){
                 if(i==0 && j==0){
