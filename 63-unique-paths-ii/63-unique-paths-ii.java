9c5436cb01e694d2e4a249cb905c9224e066727b
yashj-7 <63853210+yashj-7@users.noreply.github.com>
2022-03-15 17:19:25 +0530

    Time: 39 ms (5.70%), Space: 43.9 MB (5.46%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -1,27 +1,39 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int[][] t = new int[obstacleGrid.length][obstacleGrid[0].length];
-        for(int i=0;i<obstacleGrid.length;i++){
-            Arrays.fill(t[i],-1);
-        }
-        return solve(obstacleGrid.length-1,obstacleGrid[0].length-1,obstacleGrid,t);
+        return solve(obstacleGrid,t);
     }
-    public static int solve(int i,int j,int[][] grid,int[][] dp){
-        if(i<0 || j<0){
-            return 0;
-        }
-        if(grid[i][j]==1){
-            return 0;
-        }
-        if(i==0 && j==0){
-            return 1;
-        }
-        if(dp[i][j] !=-1){
-            return dp[i][j];
-        }
-        int up = solve(i-1,j,grid,dp);
-        int left = solve(i,j-1,grid,dp);
-        return dp[i][j]=up+left;
+    public static int solve(int[][] grid,int[][] dp){
         
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(i==0 && j==0){
+                    if(grid[0][0]==0){
+                        dp[i][j]=1;
+                    }else{
+                        dp[i][j]=0;
+                    }
+                }else if(grid[i][j]==1){
+                    dp[i][j] = 0;
+                }else{
+                    int up = 0;
+                    int left = 0;
+                    if(i>0){
+                        up = dp[i-1][j];
+                    }
+                    if(j>0){
+                        left = dp[i][j-1];
+                    }
+                    dp[i][j] = up+left;
+                }
+            }
+        }
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                System.out.print(dp[i][j]+" ");
+            }
+            System.out.println();
+        }
+        return dp[grid.length-1][grid[0].length-1];
     }
 }
