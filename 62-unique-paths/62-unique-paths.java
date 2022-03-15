84fe1c919af87ea38fa5bf6834f918a5b598981e
yashj-7 <63853210+yashj-7@users.noreply.github.com>
2022-03-15 15:08:15 +0530

    Time: 0 ms (100.00%), Space: 38.7 MB (76.02%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,24 +1,19 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] t = new int[m+1][n+1];
-        for(int i=0;i<m;i++){
-            Arrays.fill(t[i],-1);
-        }
-        return solve(0,0,m,n,t);
+        int[][] t = new int[m][n+1];
+        return solve(m,n,t);
     }
-    public static int solve(int i,int j,int m,int n,int[][] t){
-        if(t[i][j]!=-1){
-            return t[i][j];
+    public static int solve(int m,int n,int[][] t){
+        
+        for(int i=0;i<=n;i++){
+            t[0][i] = 1;
         }
-        if(i==m-1 || j==n-1){
-            return t[i][j]=1;
+        for(int i=1;i<m;i++){
+            for(int j=1;j<=n;j++){
+                t[i][j] = t[i-1][j]+t[i][j-1];
+            }
         }
-        if(i>=m || j>=n){
-            return t[i][j]=0;
-        }
-        //System.out.print(i,j);
-        int l = solve(i+1,j,m,n,t);
-        int r = solve(i,j+1,m,n,t);
-        return t[i][j]=l+r;
+        return t[m-1][n];
+        
     }
 }
