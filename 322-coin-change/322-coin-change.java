a4dc02da7c4862051c89ba09b52b0e8beca42ca1
yashj-7 <63853210+yashj-7@users.noreply.github.com>
2022-03-21 12:06:55 +0530

    Time: 12 ms (91.96%), Space: 42.5 MB (75.28%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -2,16 +2,36 @@ class Solution {
     public int coinChange(int[] coins, int amount) {
         
         int[][] dp = new int[coins.length+1][amount+1];
-        for(int i=0;i<coins.length+1;i++){
-            Arrays.fill(dp[i],-1);
+        for(int i=0;i<amount+1;i++){
+            dp[0][i] =(int)1e9;
         }
-        
-        int ans = solve(coins,amount,coins.length-1,dp);
-        
-        if(ans>=(int)1e9){
+        for(int i=0;i<coins.length+1;i++){
+            dp[i][0] = 0;
+        }
+        dp[0][0] = 1;
+        for(int i=1;i<coins.length+1;i++){
+            for(int j=1;j<amount+1;j++){
+                int x1 = (int)1e9;
+                int x2 = (int)1e9;
+                if(coins[i-1]>j){
+                    x1 = dp[i-1][j];
+                }else{
+                    x2 = 1+dp[i][j-coins[i-1]];
+                    x1 = dp[i-1][j];
+                }
+                dp[i][j] = Math.min(x1,x2);
+            }
+        }
+        if(dp[coins.length][amount]>=(int)1e9){
             return -1;
         }
-        return ans;
+        return dp[coins.length][amount];
+//         int ans = solve(coins,amount,coins.length-1,dp);
+        
+//         if(ans>=(int)1e9){
+//             return -1;
+//         }
+//         return ans;
     }
     public int solve(int[] coins,int amount,int index,int[][] dp){
         if(amount<0){
