563fcb656f634f4f888df75b133eb6ea0464ce0b
yashj-7 <63853210+yashj-7@users.noreply.github.com>
2022-03-23 17:09:09 +0530

    Time: 17 ms (66.02%), Space: 46.5 MB (34.55%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,10 +1,25 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         int[][] dp = new int[text1.length()+1][text2.length()+1];
-        for(int i=0;i<dp.length;i++){
-            Arrays.fill(dp[i],-1);
+        
+        for(int i=0;i<text1.length()+1;i++){
+            dp[i][0] = 0; 
         }
-        return solve(text1,text2,text1.length()-1,text2.length()-1,dp);
+        for(int i=0;i<text2.length()+1;i++){
+            dp[0][i] = 0;
+        }
+        dp[0][0] =0;
+        for(int i=1;i<dp.length;i++){
+            for(int j=1;j<dp[0].length;j++){
+                if(text1.charAt(i-1)==text2.charAt(j-1)){
+                    dp[i][j] = 1+dp[i-1][j-1];
+                }else{
+                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        
+        return dp[text1.length()][text2.length()];
     }
     public int solve(String s1,String s2,int ind1,int ind2,int[][] dp){
         if(ind1<0 || ind2<0){
