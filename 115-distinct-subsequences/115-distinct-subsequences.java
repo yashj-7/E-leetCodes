50cb5dac56c3d12c1a563289979260755407bce1
yashj-7 <63853210+yashj-7@users.noreply.github.com>
2022-03-24 17:03:53 +0530

    Time: 24 ms (64.30%), Space: 50 MB (54.19%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.java
+++ 115-distinct-subsequences/115-distinct-subsequences.java
@@ -1,18 +1,28 @@
 class Solution {
     public int numDistinct(String s, String t) {
         int[][] dp = new int[s.length()+1][t.length()+1];
-        for(int i=0;i<s.length()+1;i++){
-            for(int j=0;j<t.length()+1;j++){
-                dp[i][j] =-1;
-            }
+        for(int i=0;i<dp[0].length;i++){
+            dp[0][i] = 0;
         }
-        return solve(s,t,0,0,dp);
+        for(int i=0;i<dp.length;i++){
+            dp[i][0] = 1;
+        }
+       for(int i=1;i<dp.length;i++){
+           for(int j=1;j<dp[0].length;j++){
+               if(s.charAt(i-1)==t.charAt(j-1)){
+                   dp[i][j] = dp[i-1][j-1]+dp[i-1][j];
+               }else{
+                   dp[i][j] = dp[i-1][j];
+               }
+           }
+       }
+        return dp[s.length()][t.length()];
     }
     public static int solve(String s, String t,int ind1,int ind2,int[][] dp){
-        if(ind2>t.length()-1){
+        if(ind2<0){
             return 1;
         }
-        if(ind1>s.length()-1){
+        if(ind1<0){
             return 0;
         }
         if(dp[ind1][ind2]!=-1){
@@ -21,10 +31,10 @@ class Solution {
         int pick =0;
         int notpick = 0;
         if(s.charAt(ind1)==t.charAt(ind2)){
-            pick = solve(s,t,ind1+1,ind2+1,dp);
-            notpick = solve(s,t,ind1+1,ind2,dp);
+            pick = solve(s,t,ind1-1,ind2-1,dp);
+            notpick = solve(s,t,ind1-1,ind2,dp);
         }else{
-            notpick = solve(s,t,ind1+1,ind2,dp);    
+            notpick = solve(s,t,ind1-1,ind2,dp);    
         }
         
         return dp[ind1][ind2]=pick+notpick;
