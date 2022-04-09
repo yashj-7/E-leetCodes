362e54c36d3f8a7119519259c2499b15c6c3b7bf
yashj-7 <63853210+yashj-7@users.noreply.github.com>
2022-04-09 19:07:05 +0530

    Time: 20 ms (37.46%), Space: 50.8 MB (29.21%) - LeetHub

---
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -0,0 +1,27 @@
+public class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int n: nums){
+            map.put(n, map.getOrDefault(n,0)+1);
+        }
+        TreeMap<Integer, List<Integer>> freqMap = new TreeMap<>();
+        for(int num : map.keySet()){
+           int freq = map.get(num);
+           if(!freqMap.containsKey(freq)){
+               freqMap.put(freq, new LinkedList<>());
+           }
+           freqMap.get(freq).add(num);
+        }
+        
+        List<Integer> res = new ArrayList<>();
+        while(res.size()<k){
+            Map.Entry<Integer, List<Integer>> entry = freqMap.pollLastEntry();
+            res.addAll(entry.getValue());
+        }
+        int[] result = new int[k];
+        for(int i=0;i<res.size();i++){
+            result[i] = res.get(i);
+        }
+        return result;
+    }
+}
