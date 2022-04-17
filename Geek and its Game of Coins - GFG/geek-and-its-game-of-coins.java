5313365c745f833bc34ba828b771ee20dcc0d919
yashj-7 <63853210+yashj-7@users.noreply.github.com>
2022-04-17 17:18:20 +0530

    Added solution - LeetHub

---
+++ Geek and its Game of Coins - GFG/geek-and-its-game-of-coins.java
@@ -0,0 +1,55 @@
+// { Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String input[] = br.readLine().split(" ");
+            int N = Integer.parseInt(input[0]);
+            int X = Integer.parseInt(input[1]);
+            int Y = Integer.parseInt(input[2]);
+            Solution ob = new Solution();
+            int ans = ob.findWinner(N,X,Y);
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution
+{
+    public int findWinner(int N,int X,int Y)
+    {
+        boolean[] dp = new boolean[N+1];
+        dp[0] = false;
+        dp[1] = true;
+        for(int i=2;i<N+1;i++){
+            if(i-1>=0){
+                dp[i] = !dp[i-1];
+            }
+            if(dp[i]==true){
+                continue;
+            }
+            if(i-X>=0){
+                dp[i] = !dp[i-X];
+            }
+            if(dp[i]==true){
+                continue;
+            }
+            if(i-Y>=0){
+                dp[i] = !dp[i-Y];
+            }
+            if(dp[i]==true){
+                continue;
+            }
+        }
+        return dp[N]==true?1:0;
+    }
+}
