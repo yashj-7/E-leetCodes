6051b96925b21ab167795ef6db1f2bd105f2c98d
yashj-7 <63853210+yashj-7@users.noreply.github.com>
2022-04-19 02:15:51 +0530

    Time: 5 ms (30.25%), Space: 41.1 MB (71.88%) - LeetHub

---
+++ 486-predict-the-winner/486-predict-the-winner.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean PredictTheWinner(int[] nums) {
+       int[][] dp = new int[nums.length][nums.length];
+        int k =0;
+        while(k<nums.length){
+            int j =k;
+            int i =0;
+            while(i<nums.length && j<nums.length){
+                if(i==j){
+                    dp[i][j] = nums[i];
+                }else if(Math.abs(j-i)==1){
+                    dp[i][j] = Math.max(nums[i],nums[j]);
+                }else{
+                    dp[i][j] = Math.max(nums[i]+Math.min(dp[i+2][j],dp[i+1][j-1]),nums[j]+Math.min(dp[i][j-2],dp[i+1][j-1]));
+                }
+                i++;
+                j++;
+            }
+            k++;
+        }
+        int sum = 0;
+        for(int i=0;i<nums.length;i++){
+            sum = sum+nums[i];
+        }
+        int player1 = dp[0][nums.length-1];
+        int player2 = sum-player1;
+        System.out.print(player1);
+        if(player2>player1){
+            return false;
+        }
+        return true;
+    }
+}
