482e88233b5840490f1ca4305cb741e0cddef058
yashj-7 <63853210+yashj-7@users.noreply.github.com>
2022-04-20 01:07:39 +0530

    Added solution - LeetHub

--- Mobile numeric keypad - GFG/mobile-numeric-keypad.java
+++ Mobile numeric keypad - GFG/mobile-numeric-keypad.java
@@ -30,17 +30,6 @@ class Solution
 	        {0,8},{1,2,4},{1,2,3,5},{2,3,6},{1,5,4,7},{2,4,5,6,8},{3,6,5,9},{4,7,8},{5,7,8,9,0},{6,9,8}
 	    };
 	    
-// 		ArrayList<Integer>[] data = new ArrayList[10];
-// 		data[0] = new ArrayList<>(Arrays.asList({0,8})));
-// 		data[1] = new ArrayList<>(Arrays.asList({1,2,4}));
-// 		data[2] = new ArrayList<>(Arrays.asList({1,2,3,5}));
-// 		data[3] = new ArrayList<>(Arrays.asList({2,3,6}));
-// 		data[4] = new ArrayList<>(Arrays.asList({1,5,4,7}));
-// 		data[5] = new ArrayList<>(Arrays.asList({2,4,5,6,8}));
-// 		data[6] = new ArrayList<>(Arrays.asList({3,6,5,9}));
-// 		data[7] = new ArrayList<>(Arrays.asList({4,7,8}));
-// 		data[8] = new ArrayList<>(Arrays.asList({5,7,8,9,0}));
-// 		data[9] = new ArrayList<>(Arrays.asList({6,9,8}));
 		long[][] dp = new long[N+1][10];
 		for(int i=0;i<10;i++){
 		    dp[0][i]=0;
