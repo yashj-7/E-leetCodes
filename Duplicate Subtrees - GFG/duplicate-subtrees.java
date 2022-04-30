7ad5f66aa3f940c2f4179d43e4467a1303ee51b0
yashj-7 <63853210+yashj-7@users.noreply.github.com>
2022-04-30 12:56:27 +0530

    Added solution - LeetHub

---
+++ Duplicate Subtrees - GFG/duplicate-subtrees.java
@@ -0,0 +1,160 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+import java.lang.*; 
+
+
+class Node {
+    int data;
+    Node left, right;
+    
+    public Node(int data){
+        this.data = data;
+    }
+}
+
+class GFG {
+    
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    public static void preorder(Node root){
+        if(root == null)
+            return;
+        System.out.print(root.data+" ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t=Integer.parseInt(br.readLine());
+		while(t-- > 0){
+		    String s = br.readLine();
+	    	Node root = buildTree(s);
+		    Solution obj = new Solution();
+		    List<Node> res = obj.printAllDups(root);
+		    for(int i = 0;i<res.size();i++){
+		        preorder(res.get(i));
+		        System.out.println();
+		    }
+		    
+		}
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*
+class Node {
+    int data;
+    Node left, right;
+    
+    public Node(int data){
+        this.data = data;
+    }
+}
+*/
+
+class Solution{
+   
+    public List<Node> printAllDups(Node root)
+    {
+       ArrayList<Node> list = new ArrayList<>();
+       HashMap<String,Integer> hm = new HashMap<>();
+       solve(root,hm,list);
+        //Collections.sort(list);
+        return list;
+    }
+    // public static String solve(HashMap<String,Integer>hm ,ArrayList<Node> list,Node root){
+    //     if(root==null){
+    //         return "";
+    //     }
+    //     String l = solve(hm,list,root.left);
+    //     String r = solve(hm,list,root.right);
+    //     String str = l+root.data+r;
+    //     if(hm.containsKey(str)){
+    //         list.add(root);
+    //     }else{
+    //         hm.put(str,1);
+    //     }
+    //     return str;
+    // }
+    public static String solve(Node root,HashMap<String,Integer> hm,ArrayList<Node> list){
+        if(root==null){
+            return "";
+        }
+        // if(root.left==null && root.right==null){
+        //     return String.valueOf(root.data);
+        // }
+        String s = String.valueOf(root.data)+"#"+solve(root.left,hm,list)+"#"+solve(root.right,hm,list);
+        if(hm.containsKey(s)){
+            hm.put(s,hm.get(s)+1);
+            if(hm.get(s)==2){
+                list.add(root);
+            }
+        }else{
+            hm.put(s,1);
+        }
+        return s;
+    }
+    
+}
