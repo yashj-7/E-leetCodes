5f3a73712233305ccfd652c430a2bd0a0aef424b
yashj-7 <63853210+yashj-7@users.noreply.github.com>
2022-07-30 21:37:40 +0530

    Time: 178 ms (10.57%), Space: 110 MB (13.82%) - LeetHub

--- 916-word-subsets/916-word-subsets.java
+++ 916-word-subsets/916-word-subsets.java
@@ -1,10 +1,8 @@
 class Solution {
     public List<String> wordSubsets(String[] words1, String[] words2) {
         HashMap<Character,Integer> hm1 = new HashMap<>();
-        //String com = "";
         for(int i=0;i<words2.length;i++){
             String s = words2[i];
-            // com += words2[i];
             HashMap<Character,Integer> hm = new HashMap<>();
             for(int j=0;j<s.length();j++){
                 if(hm1.containsKey(s.charAt(j))==false){
@@ -35,30 +33,7 @@ class Solution {
                     hm2.put(x.charAt(j),1);
                 }
             }
-            //System.out.println(hm2);
             int flag = 0;
-            // for(int j=0;j<com.length();j++){
-            //     if(hm2.containsKey(com.charAt(j))==false){
-            //         flag = false;
-            //         break;
-            //     }else{
-            //        if(hm2.get(com.charAt(j))<hm1.get(com.charAt(j))){
-            //            flag=false;
-            //            break;
-            //        }
-            //     }
-            // }
-            // if(flag){
-            //     list.add(x);
-            // }
-            
-            // for(int j=0;j<keys.length;j++){
-            //     System.out.println(hm1.get(keys[j]));
-            //     if(hm2.get(keys[j].toString().charAt(0))>hm1.get(keys[j].toString().charAt(0))){
-            //         flag = false;
-            //         break;
-            //     }
-            // }
             int count = 0;
             for(int j=0;j<x.length();j++){
                 if(hm1.containsKey(x.charAt(j))==true && hm2.containsKey(x.charAt(j))==true && hm1.get(x.charAt(j))<=hm2.get(x.charAt(j))){
