a826295ec5f98b3fbc7da40867e86f7b9259b8f3
yashj-7 <63853210+yashj-7@users.noreply.github.com>
2022-08-01 16:16:16 +0530

    Time: 92 ms (34.94%), Space: 44.2 MB (62.35%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,11 +1,31 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int[][] dp = new int[nums.length+1][nums.length+1];
-        for(int i=0;i<nums.length+1;i++){
-            Arrays.fill(dp[i],-1);
+        // int[][] dp = new int[nums.length+1][nums.length+1];
+        // for(int i=0;i<nums.length+1;i++){
+        //     Arrays.fill(dp[i],-1);
+        // }
+        // return solve(nums,0,-1,dp);
+        
+        
+        //tabulation:-
+        int[] dp = new int[nums.length];
+        dp[0]=1;
+        for(int i=1;i<nums.length;i++){
+            dp[i] = 1;
+            for(int j=i-1;j>=0;j--){
+                if(nums[j]<nums[i]){
+                    dp[i] = Math.max(dp[i],dp[j]+1);
+                }
+            }
         }
-        return solve(nums,0,-1,dp);
+        int ans = 0;
+        for(int i=0;i<dp.length;i++){
+            ans = Math.max(ans,dp[i]);
+        }
+        return ans;
     }
+    
+    //memoized code
     public int solve(int[] nums,int ind,int ind2,int[][] dp){
         
         if(ind==nums.length){
