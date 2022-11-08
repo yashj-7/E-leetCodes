8fc61f6e83fc9672add4e354c99497f973b30402
yashj-7 <63853210+yashj-7@users.noreply.github.com>
2022-11-08 13:16:51 +0530

    Time: 12 ms (20.10%), Space: 51.6 MB (46.14%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,39 +1,30 @@
+//intuition :- All the Os connected with Os at the boundary of the matrix cannot be converted to X . So find Os at the boundary and do a dfs traversal for the same.
 class Solution {
     public void solve(char[][] board) {
         int i=0;
         int[][] vis= new int[board.length][board[0].length];
         for(int j=0;j<board[0].length;j++){
             if(board[i][j]=='O' && vis[i][j]!=1){
-                //vis[i][j]=1;
                 dfs(i,j,vis,board);
             }
         }
         for(int j=0;j<board.length;j++){
             if(board[j][i]=='O' && vis[j][i]!=1){
-                //vis[i][j]=1;
                 dfs(j,i,vis,board);
             }
         }
         i = board.length-1;
         for(int j=0;j<board[0].length;j++){
             if(board[i][j]=='O' && vis[i][j]!=1){
-                //vis[i][j]=1;
                 dfs(i,j,vis,board);
             }
         }
         i=board[0].length-1;
         for(int j=0;j<board.length;j++){
             if(board[j][i]=='O' && vis[j][i]!=1){
-                //vis[i][j]=1;
                 dfs(j,i,vis,board);
             }
         }
-        // for(int x=0;x<board.length;x++){
-        //     for(int j=0;j<board[0].length;j++){
-        //         System.out.print(vis[x][j]+" ");
-        //     }
-        //     System.out.println();
-        // }
         for(int x=0;x<board.length;x++){
             for(int j=0;j<board[0].length;j++){
                 if(vis[x][j]==0 && board[x][j]=='O'){
